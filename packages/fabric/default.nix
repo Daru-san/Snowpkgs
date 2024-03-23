5f8b7ab0d3bbb357d5275cf09cebd7a466b522f7
Daru-san <mica.tui@proton.me>
2024-03-23 16:49:47 +0200

    pkgs/fabric: test fixes

--- packages/fabric/default.nix
+++ packages/fabric/default.nix
@@ -25,7 +25,9 @@ with python3Packages;
       rev = "9adb28d7659d9068ff05f1410767334608fa4095";
       hash = "sha256-U9lA+nht23tXoSredZEnXOzW/lTH0rr29nQF5zP9eEo=";
     };
-    propagatedBuildInputs = [
+    pyroject = true;
+    doCheck = false;
+    dependencies = [
       gtk3
       cairo
       gtk-layer-shell
@@ -42,7 +44,6 @@ with python3Packages;
       description = "Fabric is a python widgets thing framework made for *Nix based systems (Wayland and X11), using GTK+.";
       homepage = "https://github.com/Fabric-Development/";
       maintainers = with maintainers; [darumaka];
-      mainProgram = "mixxc";
       platforms = platforms.linux;
     };
   }
