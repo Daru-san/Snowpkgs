2d32c435e1c37e3875b2f629385d657e713cbd04
Daru-san <mica.tui@proton.me>
2024-03-23 16:11:19 +0200

    pkgs/fabric: test poetry2nix

--- packages/fabric/default.nix
+++ packages/fabric/default.nix
@@ -1,48 +1,21 @@
 {
+  inputs,
+  poetry2Nix ? inputs.poetry2Nix.packages.default,
   lib,
-  python3Packages,
   fetchFromGitHub,
-  gtk3,
-  cairo,
-  gtk-layer-shell,
-  gobject-introspection,
-  pip,
-  psutil,
-  pygobject3,
-  pycairo,
-  loguru,
-  click,
-  conf,
 }:
-with python3Packages;
-  buildPythonApplication {
-    pname = "fabric";
-    version = "0.0.1";
-    src = fetchFromGitHub {
-      owner = "Fabric-Development";
-      repo = "fabric";
-      rev = "9adb28d7659d9068ff05f1410767334608fa4095";
-      hash = "sha256-U9lA+nht23tXoSredZEnXOzW/lTH0rr29nQF5zP9eEo=";
-    };
-    propagatedBuildInputs = [
-      gtk3
-      cairo
-      gtk-layer-shell
-      gobject-introspection
-      pip
-      pygobject3
-      pycairo
-      loguru
-      click
-      psutil
-      conf
-    ];
-    meta = with lib; {
-      description = "Mixxc is a minimalistic and customizable volume mixer, created to seamlessly complement desktop widgets.";
-      homepage = "https://github.com/Elvyria/mixxc";
-      license = with licenses; [mit];
-      maintainers = with maintainers; [darumaka];
-      mainProgram = "mixxc";
-      platforms = platforms.linux;
-    };
-  }
+poetry2Nix.mkPoetryApplication {
+  projectDir = fetchFromGitHub {
+    owner = "Fabric-Development";
+    repo = "fabric";
+    rev = "9adb28d7659d9068ff05f1410767334608fa4095";
+    hash = "sha256-U9lA+nht23tXoSredZEnXOzW/lTH0rr29nQF5zP9eEo=";
+  };
+  meta = with lib; {
+    description = "Fabric is a python widgets thing framework made for *Nix based systems (Wayland and X11), using GTK+.";
+    homepage = "https://github.com/Fabric-Development/Fabric";
+    maintainers = with maintainers; [darumaka];
+    mainProgram = "mixxc";
+    platforms = platforms.linux;
+  };
+}
