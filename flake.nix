6ea33a7e15ba7066812cea812bc0a6625af41e4a
Daru-san <mica.tui@proton.me>
2024-03-23 15:08:52 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A flake for packages I use that aren't packaged in nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
+      src = ./.;
+    };
+}
