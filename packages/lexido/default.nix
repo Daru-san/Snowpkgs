7f5d787cf8feaf67af623fb0b7a08a4e8b1c6ae2
daru <mica.tui@proton.me>
2024-05-21 19:56:44 +0200

    lexido: 1.4 -> 1.4.2
    Diff: https://github.com/micr0-dev/lexido/compare/v1.4...v1.4.2
    
    Changelog: https://github.com/micr0-dev/lexido/releases/tag/v1.4.2

--- packages/lexido/default.nix
+++ packages/lexido/default.nix
@@ -5,14 +5,14 @@
 }:
 buildGoModule rec {
   pname = "lexido";
-  version = "1.4";
+  version = "1.4.2";
   src = fetchFromGitHub {
     owner = "micr0-dev";
     repo = "lexido";
     rev = "v${version}";
-    hash = "sha256-7T/5lWMjpzVGNqKBWcJ/Ux7rhtjIepz/a+aWmB8Z+Do=";
+    hash = "sha256-nc6UvW16MmLsKt0oSb9nG64N7J3+5CveSwPnGOezhGY=";
   };
-  vendorHash = "sha256-2FsGCCWRHllHowl6T0Nhp/KZfwNNe/3IL6PsMWzbiHI=";
+  vendorHash = "sha256-h3ws9k7W4nNyS1WvZP29NJfJsBOe0D47ykd41C96Xi4=";
   meta = with lib; {
     description = "An innovative assistant for the command line";
     homepage = "https://github.com/micr0-dev/lexido";
