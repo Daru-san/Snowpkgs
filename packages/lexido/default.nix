dd3a9ef828a8fff2f936dc78eec1080a83492171
daru <mica.tui@proton.me>
2024-05-21 20:52:27 +0200

    lexido: reformat

--- packages/lexido/default.nix
+++ packages/lexido/default.nix
@@ -1,25 +1,28 @@
-{
-  buildGoModule,
-  fetchFromGitHub,
-  lib,
+{ lib
+, buildGoModule
+, fetchFromGitHub
 }:
+
 buildGoModule rec {
   pname = "lexido";
   version = "1.4.2";
+
   src = fetchFromGitHub {
     owner = "micr0-dev";
     repo = "lexido";
     rev = "v${version}";
     hash = "sha256-nc6UvW16MmLsKt0oSb9nG64N7J3+5CveSwPnGOezhGY=";
   };
+
   vendorHash = "sha256-h3ws9k7W4nNyS1WvZP29NJfJsBOe0D47ykd41C96Xi4=";
+
+  ldflags = [ "-s" "-w" ];
+
   meta = with lib; {
-    description = "An innovative assistant for the command line";
+    description = "A terminal assistant, powered by Generative AI";
     homepage = "https://github.com/micr0-dev/lexido";
-    changelog = "https://github.com/micr0-dev/lexido/releases/tag/v${version}";
-    license = with licenses; [mit];
-    maintainers = with maintainers; [daru-san];
+    license = licenses.agpl3Only;
+    maintainers = with maintainers; [ ];
     mainProgram = "lexido";
-    platforms = platforms.linux;
   };
 }
