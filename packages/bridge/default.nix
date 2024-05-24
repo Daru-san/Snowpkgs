897a962c3e33314d3c2ab7ebc2bafa4fb0397e8f
daru <mica.tui@proton.me>
2024-05-24 07:58:47 +0200

    bridge-editor: 2.7.18 -> 2.7.19
    Diff: https://github.com/bridge-core/editor/compare/None...v2.7.19

--- packages/bridge/default.nix
+++ packages/bridge/default.nix
@@ -9,11 +9,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "bridge-editor";
-  version = "2.7.18";
+  version = "2.7.19";
 
   src = fetchurl {
     url = "https://github.com/bridge-core/editor/releases/download/v${version}/bridge_${version}_amd64.deb";
-    hash = "sha256-VxsT0DYO4FCDSmRJAyBwIISWc7vYPvGj4mP1u1p3reo=";
+    hash = "sha256-S7V2jf3+2bMqufvxH9v+VTmVECwMorFNhpVEhsnRmh8=";
   };
 
   nativeBuildInputs = [
