9352188696a1ce4f64626dafaf1e99840017fc75
Daru <mica.tui@proton.me>
2024-07-27 09:41:29 +0200

    packages/mangayomi: attempt
    from nixpkgs pr #286512

--- packages/mangayomi/default.nix
+++ packages/mangayomi/default.nix
@@ -1,57 +1,146 @@
 {
-  stdenv,
   lib,
+  stdenv,
   fetchurl,
-  autoPatchelfHook,
-  mpv,
-  unzip,
-  gtk3,
-  pango,
-  cairo,
+  fetchFromGitHub,
   flutter,
-  wrapGAppsHook4,
-  jre_minimal,
+  jq,
+  moreutils,
+  protobuf,
+  protoc-gen-prost,
+  protoc-gen-dart,
+  cargo,
+  rustc,
+  rustPlatform,
+  mpv-unwrapped,
+  webkitgtk_4_1,
+  xdg-user-dirs,
+  gnome,
+  jdk,
 }:
-stdenv.mkDerivation rec {
-  pname = "mangayomi";
-  version = "0.2.7";
-
-  src = fetchurl {
-    url = "https://github.com/kodjodevf/mangayomi/releases/download/v${version}/Mangayomi-v${version}-linux.zip";
-    hash = "sha256-z+KS7YF5Op4OxiGswpB6P6ysDgWDYKbAGoB6YMTnUbQ=";
+let
+  # Used inside media-kit
+  # https://github.com/media-kit/media-kit/blob/main/libs/linux/media_kit_libs_linux/linux/CMakeLists.txt
+  mimalloc-version = "2.1.2";
+  mimalloc-tar = fetchurl {
+    url = "https://github.com/microsoft/mimalloc/archive/refs/tags/v${mimalloc-version}.tar.gz";
+    hash = "sha256-Kxv/b3F/lyXHC/jXnkeG2hPeiicAWeS6C90mKue+Rus=";
   };
-  dontUnpack = true;
-  nativeBuildInputs = [
-    autoPatchelfHook
-    unzip
-    wrapGAppsHook4
-  ];
 
-  builtInputs = [
-    gtk3
-    jre_minimal
-    pango
-    cairo
-    flutter
-  ];
-  runtimeDependencies = [ mpv ];
+  buildInfix =
+    {
+      "x86_64-linux" = "linux/x64";
+      "aarch64-linux" = "linux/arm64";
+      "x86_64-darwin" = "macos/x64";
+      "aarch64-darwin" = "macos/arm64";
+    }
+    .${stdenv.system} or (throw "Unsupported system");
 
-  sourceRoot = ".";
+  mainPubspecLock = lib.importJSON ./pubspec.lock.json;
+  cargokitBuildToolPubspecLock = lib.importJSON ./cargokit-build-tool-pubspec.lock.json;
+in
+flutter.buildFlutterApplication rec {
+  pname = "mangayomi";
+  version = "0.2.75-unstable-2024-07-17";
 
-  installPhase = ''
-    mkdir -p $out
-    unzip $src
-    cp -avr lib $out/lib
-    install -m755 -D mangayomi $out/bin/mangayomi
+  src = fetchFromGitHub {
+    owner = "kodjodevf";
+    repo = "mangayomi";
+    rev = "v${version}";
+    hash = "sha256-yrLaytLdGt7UIcAsLxmDks90uSFgfwFuoigcRCGLj78=";
+  };
+
+  postPatch = ''
+    substituteInPlace linux/appimage/AppRun.desktop \
+        --replace-fail "AppRun" "mangayomi"
   '';
 
-  meta = with lib; {
-    description = "Manga reader and anime streamer";
+  customSourceBuilders = {
+    rinf =
+      { version, src, ... }:
+      stdenv.mkDerivation {
+        pname = "rinf";
+        inherit version src;
+        inherit (src) passthru;
+
+        patches = [ ./rinf.patch ];
+
+        installPhase = ''
+          runHook preInstall
+          mkdir -p "$out"
+          cp -r * "$out"
+          runHook postInstall
+        '';
+      };
+  };
+
+  # build_tool hack part 1: join dependencies with the main package
+  pubspecLock = lib.recursiveUpdate cargokitBuildToolPubspecLock mainPubspecLock;
+
+  gitHashes = {
+    flutter_windows_webview = "sha256-rVu+qQJxOQG+LDFCSO3ueg3aHmIFhPK2H51FTHhLTlg=";
+    media_kit_video = "sha256-Kx0rr4x6sxPgfX3C5jjr8VYq0X/pPnjNDI/F/d41rVk=";
+    flutter_qjs = "sha256-l6uUUqiIkdD3ayUY9rUzxKXunlW2QU2sAuDd8fc2Iyc=";
+  };
+
+  nativeBuildInputs = [
+    jq
+    moreutils # sponge
+    protobuf
+    protoc-gen-prost
+    protoc-gen-dart
+    cargo
+    rustc
+    rustPlatform.cargoSetupHook
+  ];
+
+  buildInputs = [
+    mpv-unwrapped
+    webkitgtk_4_1
+    jdk
+  ];
+
+  cargoDeps = rustPlatform.fetchCargoTarball {
+    inherit src;
+    name = "${pname}-${version}";
+    hash = "sha256-9xUQEItdeebN/gSS3e/6fJhrqpk5cPsR7fxckiCQM4A=";
+  };
+
+  # media-kit builds mimalloc from source
+  preConfigure = ''
+    install -D ${mimalloc-tar} build/${buildInfix}/release/mimalloc-${mimalloc-version}.tar.gz
+  '';
+
+  preBuild = ''
+    # build_tool hack part 2: add build_tool as an actually resolvable package (the location is relative to the rinf package directory)
+    jq '.packages += [.packages.[] | select(.name == "rinf") | .rootUri += "/cargokit/build_tool" | .name = "build_tool"]' .dart_tool/package_config.json | sponge .dart_tool/package_config.json
+    # generate messages used by the main package
+    packageRun rinf message
+  '';
+
+  env.NIX_CFLAGS_COMPILE = "-Wno-error=int-conversion";
+
+  postInstall = ''
+    install -Dm644 linux/appimage/AppRun.desktop $out/share/applications/mangayomi.desktop
+    install -Dm644 linux/appimage/AppRun.png $out/share/pixmaps/mangayomi.png
+  '';
+
+  extraWrapProgramArgs = ''
+    --prefix LD_LIBRARY_PATH : $out/app/lib \
+    --prefix PATH : ${
+      lib.makeBinPath [
+        xdg-user-dirs
+        gnome.zenity
+      ]
+    }
+  '';
+
+  meta = {
+    changelog = "https://github.com/kodjodevf/mangayomi/releases/tag/${src.rev}";
+    description = "Free and open source application for reading manga and watching anime";
     homepage = "https://github.com/kodjodevf/mangayomi";
-    platforms = [ "x86_64-linux" ];
-    license = licenses.gpl3Plus;
-    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
-    maintainers = with maintainers; [ daru-san ];
+    license = lib.licenses.asl20;
     mainProgram = "mangayomi";
+    maintainers = with lib.maintainers; [ tomasajt ];
   };
 }
