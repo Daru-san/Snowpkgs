993ed5e139530d438f496e2a13899f42b457fc30
Daru <mica.tui@proton.me>
2024-08-03 13:00:13 +0200

    packages/gh-download: correct version

--- packages/gh-download/default.nix
+++ packages/gh-download/default.nix
@@ -5,14 +5,14 @@
 }:
 stdenv.mkDerivation rec {
   pname = "gh-download";
-  version = "0.6.1-unstable-2023-04-03";
+  version = "0.6.1";
   src = pkgs.fetchFromGitHub {
     owner = "yuler";
     repo = "gh-download";
     rev = "v${version}";
     hash = "sha256-pMKVDJDhvzMlezhs7JpxrpljuRyW8Vz2rmwpAWLxXko=";
   };
-  nativeBuildInputs = [ makeWrapper ];
+  nativeBuildInputs = [makeWrapper];
   buildPhase = ''
     mkdir -p $out/bin
     cp $src/gh-download $out/bin/gh-download
