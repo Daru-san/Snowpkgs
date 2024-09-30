57067537f6547e81447e7392c8d366bdec17b317
Daru <mica.tui@proton.me>
2024-09-30 19:24:46 +0200

    poketex: 1.14.1 -> 1.16.1
    Diff: https://github.com/ckaznable/poketex/compare/v1.14.1...v1.16.1

--- packages/poketex/default.nix
+++ packages/poketex/default.nix
@@ -7,16 +7,16 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "poketex";
-  version = "1.14.1";
+  version = "1.16.1";
 
   src = fetchFromGitHub {
     owner = "ckaznable";
     repo = "poketex";
     rev = "v${version}";
-    hash = "sha256-8f/eaNxhu5R6LQ1CisZ74+/o/sywFjZYl+v6bqeKJm8=";
+    hash = "sha256-jpnjNwwtr6n9Kha1nXeXqXW9ODnwuKW8YvGFfTWHNPk=";
   };
 
-  cargoHash = "sha256-xYScwEsyyntF7/i/Anfayh3tQ9dPAKapfdcKwpYOrzo=";
+  cargoHash = "sha256-uWEdbk/on0qLJhuBuwNtwm1c89IYkzfyFGno1LGhJCE=";
 
   buildInputs = lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.CoreFoundation ];
 
