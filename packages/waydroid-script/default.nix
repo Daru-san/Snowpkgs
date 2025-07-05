ccc10d182ec64bd6d1c990c3530fad8ccddaea42
Daru-san <Daru-san@users.noreply.github.com>
2025-07-05 04:40:57 +0000

    [create-pull-request] automated change

--- packages/waydroid-script/default.nix
+++ packages/waydroid-script/default.nix
@@ -14,13 +14,13 @@ in
 stdenvNoCC.mkDerivation rec {
   name = "waydroid_script";
 
-  version = "0-unstable-2025-05-30";
+  version = "0-unstable-2025-07-04";
 
   src = fetchFromGitHub {
     owner = "casualsnek";
     repo = "waydroid_script";
-    rev = "32d21c8074c17bfbab70f7158b80464227821424";
-    hash = "sha256-dnQBDcHM7fG+w7YilbKT2kon4cRfGprhfL3v85z4fdw=";
+    rev = "6f44d95070cd8a767f528ef3e2ed73a34ccb2a6a";
+    hash = "sha256-AiSkFcliCAmTUkGxejcJyYjotDooWxol+1LfVbEDKcY=";
   };
 
   buildInputs = [
